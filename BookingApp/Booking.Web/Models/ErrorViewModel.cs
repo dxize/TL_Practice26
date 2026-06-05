a9c0f2c7ab267165606470227d3bf069556538ae
dxize <159326372+dxize@users.noreply.github.com>
2026-06-05 14:18:45 +0300

    пока удалил лишнее

--- BookingApp/Booking.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Booking.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty( RequestId );
-    }
-}
