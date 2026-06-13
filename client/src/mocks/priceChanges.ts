931bd57224837429b3b74ca375e872b5189cbbb2
dxize <159326372+dxize@users.noreply.github.com>
2026-06-14 02:49:47 +0300

    исправил ошибку типизации старых моков через тип any

--- client/src/mocks/priceChanges.ts
+++ client/src/mocks/priceChanges.ts
@@ -1,4 +1,3 @@
-import type { PriceChange } from "../models";
 import data from "./2_hw_mock_price_changes.json";
 
-export const priceChanges: Record<string, Record<string, PriceChange>> = data;
+export const priceChanges: any = data;
